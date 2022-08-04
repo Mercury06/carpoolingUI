c1c4448e1e1b88f03cb669a8dd6a0dc5194ad392
Mercury06 <zetmail@ukr.net>
2022-08-04 21:34:42 +0300

    commit1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from "react"
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <center><h6>UI</h6></center>
+      <Header />
+      
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import s from './Header.module.css'
+const Header = () => {
+    return (
+        <div className={s.navbar}>
+            <center><h1>UI</h1></center>
+        </div>
+    )
+}
+
+export default Header;
