9ff48fffc757b26bb37e167c483f6774789177bd
Mercury06 <zetmail@ukr.net>
2022-10-21 15:30:19 +0300

    fethe rides comit

--- src/components/UserPage/UserRides.jsx
+++ src/components/UserPage/UserRides.jsx
@@ -9,66 +9,21 @@ const UserRides = () => {
   const id = user.id;
   const [rides, setRides] = useState(null);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await axios.get(`http://localhost:9000/api/settings/findmyrides/${id}`);
-  //     const data = await response.data;
-  //     if (data) {
-  //       setRides(data);
-  //     } else {
-  //       setRides([
-  //         {
-  //           localityFrom: {
-  //             localityName: 'New Orlean',
-  //             id: '6260413bf25174950eb09a52',
-  //           },
-  //           destination: {
-  //             localityName: 'Los Angeles',
-  //             id: '626e5943b749f1042b21d252',
-  //           },
-  //           _id: '63282a5dff6daa6582a18481',
-  //           user: '625474cdaeaabfaac86eda54',
-  //           seats: 1,
-  //           date: '2022-09-19T00:00:00.000Z',
-  //           completed: false,
-  //           createdAt: '2022-09-19T08:37:49.602Z',
-  //           updatedAt: '2022-09-19T08:37:49.602Z',
-  //           __v: 0,
-  //         },
-  //       ]);
-  //     }
-  //   }
-  //   fetchData().catch(console.error);
-  // }, [id]);
-
   useEffect(() => {
-    function status(response) {
-      if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response);
-      } else {
-        return Promise.reject(new Error(response.statusText));
-      }
+    async function fetchData() {
+      const response = await axios.get(
+        `https://fast-river-99451.herokuapp.com/api/settings/findmyrides/${id}`,
+      );
+      const data = response.data;
+      setRides(data);
     }
-
-    function json(response) {
-      return response.json();
-    }
-
-    fetch(`http://localhost:9000/api/settings/findmyrides/${id}`)
-      .then(status)
-      .then(json)
-      .then(function (data) {
-        console.log('Request succeeded with JSON response', data);
-        setRides(data);
-      })
-      .catch(function (error) {
-        console.log('Request failed', error);
-      });
+    fetchData().catch(console.error);
   }, [id]);
 
   return (
     <div>
       {rides && <h1>Found {rides.length} results</h1>}
+      {id && <h1>user id: {id}</h1>}
       {rides && rides.length > 0 ? (
         rides.map((item, i) => {
           return (
