b80b4ee4886eeadf4758616189a492c4a0eb2376
Mercury06 <zetmail@ukr.net>
2022-10-21 19:20:26 +0300

    commit1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './reducers';
