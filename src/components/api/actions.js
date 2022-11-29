d5d56926e57ae158f46fc78a66380a0a50695b4e
Mercury06 <zetmail@ukr.net>
2022-11-29 20:10:09 +0300

    commit1

--- src/components/api/actions.js
+++ src/components/api/actions.js
@@ -72,18 +72,6 @@ export const auth = () => {
 //       }
 // }
 
-// export const findMyRides = async (id) => {
-//   //debugger
-//   try {
-//     const response = await axios.get(`http://localhost:9000/api/settings/findmyrides/${id}`);
-//     const data = response.data;
-
-//    //     return data;
-//   } catch (e) {
-//     alert(e.response.data.message);
-//   }
-// };
-
 // export const findRidesBy = async (date) => {
 //     debugger
 //      try {
@@ -96,6 +84,18 @@ export const auth = () => {
 //          alert(e.response.data.message)
 //       }
 // }
+
+// export const findMyRides = async (id) => {
+//   //debugger
+//   try {
+//     const response = await axios.get(`http://localhost:9000/api/settings/findmyrides/${id}`);
+//     const data = response.data;
+
+//    //     return data;
+//   } catch (e) {
+//     alert(e.response.data.message);
+//   }
+// };
 
 export function findLocality(search) {
   return async (dispatch) => {
