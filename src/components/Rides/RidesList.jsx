6d5698e0256e8238a50516d3f0a52bfb71c735d4
Mercury06 <zetmail@ukr.net>
2023-02-03 13:19:40 +0300

    commit1

--- src/components/Rides/RidesList.jsx
+++ src/components/Rides/RidesList.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
-import AskForm from '../App/Forms/Search/AskForm';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import RideItem from './RideItem';
 import s from './Rides.module.scss';
-import { Button, Space } from 'antd';
+import { Button } from 'antd';
+import { createAsk } from '../api/actions';
+import cn from 'classnames';
+import { CheckIcon } from '../assets/svg/BoxIcons';
 
 const RidesList = () => {
+  const [loading, setLoading] = useState(false);
   const rides = useSelector((state) => state.ride.rides);
+  const searchRidesParams = useSelector((state) => state.ride.searchRidesParams);
+
+  const subscribeHandler = async (e) => {
+    e.stopPropagation();
+    setLoading(true);
+    //await createAsk(searchRidesParams);
+    return;
+  };
+  // const wrapperClasses = cn(s.button, {
+  //   {s.button--loading}: loading,
+  // })
 
   return (
     <div className={s.list}>
@@ -30,12 +43,18 @@ const RidesList = () => {
               <h4>
                 didn`t find a trip? just {'  '}
                 <br></br>
-                {/* <Link to="/subscribe">subscribe</Link> */}
-                <Space wrap>
-                  <Button type="primary" style={{ backgroundColor: 'gray', borderRadius: '1px' }}>
-                    Subscribe
-                  </Button>
-                </Space>
+                <Button
+                  type="primary"
+                  onClick={subscribeHandler}
+                  //style={{ backgroundColor: 'gray', borderRadius: '1px' }}
+                  //className={cn(s.button, s.success)}
+                  //className={s.button}
+                  className={cn(s.button, {
+                    [s.button__loading]: loading === true,
+                  })}
+                >
+                  <span className={s.button__text}>Subscribe</span>
+                </Button>
               </h4>
             </center>
           </div>
---
+++ src/components/assets/svg/BoxIcons.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const CheckIcon = (props) => {
+  return (
+    <svg
+      width="26px"
+      height="26px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_429_11206)">
+        <circle
+          cx="12"
+          cy="11.9999"
+          r="9"
+          stroke="#237804"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M15 10.0001L11 14.0001L9 12.0001"
+          stroke="#237804"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_429_11206">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
