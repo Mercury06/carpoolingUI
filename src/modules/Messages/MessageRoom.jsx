9f8c0a11713e9e65f7c4c0aa0005758044f579db
mercury06 <zetmail@ukr.net>
2023-07-30 20:31:20 +0300

    commit3

--- src/modules/Messages/MessageRoom.jsx
+++ src/modules/Messages/MessageRoom.jsx
@@ -7,28 +7,32 @@ import Input from './Input';
 export default function MessageRoom() {
   return (
     <div className={s.room_container}>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        {/* <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1>
-        <h1>message room</h1> */}
-        <div className={s.input_message_container}>
-        <Input />
-        <button>Send</button>
+        <div className={s.text_container}>
+            <div className={s.text_container_body}>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            <h1>message room</h1>
+            </div>
+            
         </div>
         
-        {/* <Dialog /> */}
-        {/* <TextArea className={s.ant_input}/> */}
+        <div className={s.input_message_container}>
+            <Input />
+            <button>Send</button>
+        </div>
+       
     </div>
   );
 }
