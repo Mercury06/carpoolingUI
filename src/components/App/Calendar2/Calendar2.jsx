16c1f090307f88a025ad2ef7edb22b568721e057
mercury06 <zetmail@ukr.net>
2024-03-24 19:09:42 +0300

    commit3

--- src/components/App/Calendar2/Calendar2.jsx
+++ src/components/App/Calendar2/Calendar2.jsx
@@ -1,31 +1,32 @@
 
 import React from 'react'; 
-
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 import s from './Calendar2.module.scss';
 
-const Calendar2 = (props) => {
+const Calendar2 = (props) => {    
     
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth()
-    const currentYear = currentDate.getFullYear()
-    console.log("currMonth", currentMonth)
-    console.log("currYear", currentYear)
+    const currentYear = currentDate.getFullYear()    
+    const curr_month = `${monthNames[currentMonth]}`;
+    const [shownMonthTitle, setShownMonthTitle] = React.useState(curr_month);
+    const [shownYear, setShownYear] = React.useState(currentYear);
+    // console.log("rerendered", shownMonthTitle)
 
-    const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-   
     
-    const isLeapYear = (currYear) => {
+    const isLeapYear = (shownYear) => {
         // isLeap = new Date(year, 1, 29).getMonth() == 1
-        return (currYear % 4 === 0 && currYear % 100 !== 0 && currYear % 400 !== 0) || (currYear % 100 === 0 && currYear % 400 === 0)
+        return (shownYear % 4 === 0 && shownYear % 100 !== 0 && shownYear % 400 !== 0) || (shownYear % 100 === 0 && shownYear % 400 === 0)
     }
 
-    const getFebDays = (currYear) => {
-        return isLeapYear(currYear) ? 29 : 28
+    const getFebDays = (currentYear) => {
+        return isLeapYear(currentYear) ? 29 : 28
     }
 
     const days_of_month = [
         31,
-        getFebDays(currentYear),
+        getFebDays(shownYear),
         31,
         30,
         31,
@@ -36,20 +37,47 @@ const Calendar2 = (props) => {
         31,
         30,
         31,
-    ];    
-
-    const curr_month = `${month_names[currentMonth]}`;
-
-    // const first_day = new Date(currentYear, currentMonth, 1)
-    const first_day = new Date(currentYear, 2, 1)
+    ];   
+    // console.log("shownYear try", shownYear)  
+    // console.log("shownMonthTitle try", monthNames.indexOf(shownMonthTitle))  
+    const first_day = new Date(shownYear, monthNames.indexOf(shownMonthTitle), 1)
+    console.log("first_day", first_day)    
     
     let daysToRender = [];
 
-    for (let i = 0; i <= days_of_month[currentMonth] + first_day.getDay() - 1; i++) {        
+    // for (let i = 0; i <= days_of_month[shownMonthTitle] + first_day.getDay() - 1; i++) {        
+    //     daysToRender.push(i - first_day.getDay() + 1)       
+    // }
+
+    for (let i = 0; i <= days_of_month[monthNames.indexOf(shownMonthTitle)] + first_day.getDay() - 1; i++) {        
         daysToRender.push(i - first_day.getDay() + 1)       
     }
 
-    // let month_list = calendar.querySelector('.month-list')
+    const onForwardMonth = () => {
+        const monthIndex = monthNames.indexOf(shownMonthTitle)
+        console.log("monthIndex", monthIndex)
+        // console.log("forward", monthNames[monthNames.indexOf(shownMonthTitle) + 1])
+        if (monthNames.indexOf(shownMonthTitle) === 11) {
+            console.log("index 11")
+            setShownYear(shownYear + 1)
+            setShownMonthTitle(monthNames[0])
+        } else {
+            setShownMonthTitle(monthNames[monthNames.indexOf(shownMonthTitle) + 1])
+        }        
+    }
+
+    const onBackMonth = () => {
+        const monthIndex = monthNames.indexOf(shownMonthTitle)
+        console.log("monthIndex", monthIndex)
+        // console.log("forward", monthNames[monthNames.indexOf(shownMonthTitle) + 1])
+        if (monthNames.indexOf(shownMonthTitle) === 0) {
+            console.log("index 0")
+            setShownYear(shownYear - 1)
+            setShownMonthTitle(monthNames[11])
+        } else {
+            setShownMonthTitle(monthNames[monthNames.indexOf(shownMonthTitle) - 1])
+        } 
+    }
 
     // month_names.forEach((e, index) => {
     //     let month = document.createElement('div')
@@ -60,38 +88,33 @@ const Calendar2 = (props) => {
     //         generateCalendar(index, curr_year.value)
     //     }
     //     month_list.appendChild(month)
-    // })   
+    // }) 
     
-    
-    const showMonthes = () => {
-        // month_list.classList.add('show')
-    }
-
-
-    // document.querySelector('#prev-year').onclick = () => {
-    //     --curr_year.value
-    //     generateCalendar(curr_month.value, curr_year.value)
-    // }
-
-    // document.querySelector('#next-year').onclick = () => {
-    //     ++curr_year.value
-    //     generateCalendar(curr_month.value, curr_year.value)
-    // }
 
     return (
         <>
             <div class={s.calendar}>
                 <div class={s.calendar_header}>
-                    <span class="month-picker" id="month-picker" onClick={showMonthes}>{curr_month}</span>
-                        <div class="year-picker">
-                            <span class="year-change" id="prev-year">
-                                <pre>prev</pre>
-                            </span>
-                            <span id="year">{currentYear}</span>
-                            <span class="year-change" id="next-year">
-                                <pre>next</pre>
+                    <div class={s.month_picker} id="month-picker">
+                        <div onClick={onBackMonth}>
+                            <span>
+                                <h3><MdArrowBackIosNew /></h3>
                             </span>
                         </div>
+                        <div class={s.month_and_year}>
+                            <div>
+                                <span><h3>{shownMonthTitle}</h3></span>
+                            </div>
+                            <div className={s.year}>
+                                <span><h3>{shownYear}</h3></span>
+                            </div>
+                        </div>
+                        <div onClick={onForwardMonth}>
+                            <span>
+                                <h3><MdArrowForwardIos /></h3>
+                            </span>
+                        </div>                        
+                    </div>     
                 </div>
                 <div class={s.calendar_body}>
                     <div class={s.calendar_week_day}>
@@ -108,18 +131,8 @@ const Calendar2 = (props) => {
                             <div>{day > 0 && day}</div>
                         ))}
                     </div>
-                </div>
-                {/* <div class="calendar-footer">
-                    <div class="toggle">
-                        <span>Dark Mode</span>
-                        <div class="dark-mode-switch">
-                            <div class="dark-mode-switch-ident"></div>
-                        </div>
-                    </div>
-                </div>
-                 <div class="month-list"></div> */}
-            </div>
-        
+                </div>               
+            </div>        
         </>
     )
 }
